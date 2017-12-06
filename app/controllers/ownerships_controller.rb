602c921ccac1775007c3cd618793b31a1cf1ad6d
rnak <rhvx3@mail.umsl.edu>
2017-12-06 10:28:19 +0000

    for heroku

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -14,7 +14,7 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
-    else if params[:type] == 'Have'
+    elsif params[:type] == 'Have'
       current_user.have(@item)
       flash[:success] = '商品を Have しました。'
     end
@@ -28,7 +28,7 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
-    else if params[:type] == 'Have'
+    elsif params[:type] == 'Have'
       current_user.unhave(@item)
       flash[:success] = '商品の Have を解除しました。'
     end
